8e4484074661de0c71f66a32a70b494e00bdc007
Иван <ivan1990kalinin@yandex.ru>
2024-07-02 19:20:22 +0700

    9.1. Тестирование. Подготовка данных. [#182960 #503201]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -38,6 +38,6 @@ class StartUITest {
         String[] answers = {String.valueOf(item.getId())};
         StartUI.deleteItem(new MockInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
-        assertThat(deleted).isEqualTo(null);
+        assertThat(deleted).isNull();
     }
 }
