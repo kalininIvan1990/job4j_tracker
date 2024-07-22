e0004650efac9cb20078b049faec918774eb2e74
Иван <ivan1990kalinin@yandex.ru>
2024-07-22 19:20:29 +0700

    5. Очередь с приоритетом на LinkedList [#41670 #506152]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,12 +8,11 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() <= task.getPriority()) {
-                index++;
+            if (element.getPriority() > task.getPriority()) {
                 break;
             }
-        }
-        this.tasks.add(index, task);
+            index++;
+        } this.tasks.add(index, task);
     }
 
     public Task take() {
