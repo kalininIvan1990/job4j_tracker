46c0c6568bad0239bb11426114abdc7e492c4ba1
Иван <ivan1990kalinin@yandex.ru>
2024-07-25 18:41:45 +0700

    2. Интерфейс Deque [#504943]

---
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -0,0 +1,37 @@
+package ru.job4j.queue;
+
+import java.util.*;
+
+public class ReconstructPhrase {
+    private final Deque<Character> descendingElements;
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Character> list = new ArrayList<>(evenElements);
+        for (int index = 0; index < list.size(); index++) {
+            if (index % 2 == 0) {
+                stringBuilder.append(list.get(index));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder stringBuilder = new StringBuilder();
+        Iterator<Character> iterator = descendingElements.descendingIterator();
+        while (iterator.hasNext()) {
+            stringBuilder.append(iterator.next());
+        }
+        return stringBuilder.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}
