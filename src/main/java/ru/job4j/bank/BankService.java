f1ced173dc7acf7b85e9476baf41f28aecb8c81f
Иван <ivan1990kalinin@yandex.ru>
2024-08-25 14:57:10 +0700

    3. Банковские переводы [#10038 #510385]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -13,10 +13,7 @@ public class BankService {
     }
 
     public void deleteUser(String passport) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            users.remove(user);
-        }
+        users.remove(new User(passport, ""));
     }
 
     public void addAccount(String passport, Account account) {
