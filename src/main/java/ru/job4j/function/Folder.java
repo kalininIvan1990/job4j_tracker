6e16554408c07297e0564d71179a9bc5384bfa29
Иван <ivan1990kalinin@yandex.ru>
2024-08-31 18:09:37 +0700

    2.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/function/Folder.java
@@ -0,0 +1,44 @@
+package ru.job4j.function;
+
+import java.util.Objects;
+
+public class Folder {
+    private String name;
+    private int size;
+
+    public Folder(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Folder folder = (Folder) object;
+        return size == folder.size && Objects.equals(name, folder.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, size);
+    }
+
+    @Override
+    public String toString() {
+        return "Folder{" + "name='" + name + '\''
+                + ", size=" + size + '}';
+    }
+}
